b251a6fa47b799ab0930779c3785cbaae7821d16
mrsep01 <sep.alamouti@sepalamouti.com>
2024-01-20 23:16:40 -0400

    initial-commit

---
+++ src/content_option.js
@@ -0,0 +1,146 @@
+const logotext = "SEP ALAMOUTI";
+const meta = {
+    title: "Sep Alamouti",
+    description: "I am Sep Alamouti, a data scientist, Full stack devlope & Product Manager,currently in NB Canada",
+};
+
+const introdata = {
+    title: "I am SEP ALAMOUTI ",
+    animated: {
+        first: "I am a Full Stack Developer",
+        second: " a product manager,", 
+        third: "Instructional designer ",
+        fourth: "I code, lead teams & design products ",
+        fifth: "I develop web applications ",
+    },
+    description: " I develop with  React, Javascript, Jquery, HTML, CSS, Bootstrap, MaterialUI, SQL, Postgres, MongoDB, Git, Github, Heroku, AWS, and more.",
+    your_img_url: "https://storage.googleapis.com/sepsimagerepo/IMG_4306.png",
+};
+
+const dataabout = {
+    title: "a little about me",
+    aboutme: "I am a Full Stack Developer, a product manager, and an instructional designer. My passion is to create products that are useful, usable, and beautiful. I have a background in science and education. I have taught science and math for 5 years. I have a master's degree in instructional design and technology. I have worked as an instructional designer for 2 years. I have a certificate in full-stack development from the University of New",
+};
+const worktimeline = [{
+        jobtitle: "Director of developer relations",
+        where: "mimik",
+        date: "2020-2023",
+    },
+    {
+        jobtitle: "MYP & DP Science Teacher",
+        where: "Beanstalk International Bilingual School",
+        date: "2018-2020",
+    },
+    {
+        jobtitle: "Math & Science Teacher",
+        where: "Anglo Singapore International School",
+        date: "2018",
+    },
+];
+
+const skills = [{
+        name: "Javsacript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 85,
+    },
+    {
+        name: "HTML",
+        value: 100,
+    },
+    {
+        name: "CSS",
+        value: 100,
+    },
+    {
+        name: "Node",
+        value: 85,
+    },
+
+    {
+        name: "MongoDB",
+        value: 85,
+    },
+
+    {
+        name: "SQL",
+        value: 85,
+    },
+
+    {
+        name: "Python",
+        value: 85,
+    },
+
+
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://storage.googleapis.com/sepsimagerepo/Screenshot%202024-01-20%20at%2015.33.27.jpg",
+        description: "React developer portal for mimiK hybrid edge cloud platform",
+        link: "https://developer.mimik.com",
+    },
+   
+    {
+        img: "https://storage.googleapis.com/sepsimagerepo/Screenshot%202024-01-20%20at%2020.41.19.jpg",
+        description: "BigBinge is where you can find your next Binge worthy series.",
+        link: "https://bhansi.github.io/BigBinge/",
+    },
+    {
+        img: "https://storage.googleapis.com/sepsimagerepo/Screenshot%202024-01-20%20at%2021.46.21.jpg",
+        description: " Kaizen-esque: A MERN stack App  that allows users to create course and enroll .",
+        link: "https://kaizen-esque.onrender.com/",
+    },
+
+    {
+        img:"https://storage.googleapis.com/sepsimagerepo/Screenshot%202024-01-20%20at%2017.38.59.jpg",
+        description: "Gatsby based Devdocs for mimik hybrid edge cloud platform ",
+        link: "https://devdocs.mimik.com",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sep.alamouti@sepalamouti.com",
+    YOUR_FONE: "+1 604 404 2436",
+    YOUR_TEMPLATE_ID: "template_cpgvhgx",
+    YOUR_USER_ID: "K1R27_IJ07qac0e5H",
+};
+
+const socialprofils = {
+    github: "https://github.com/mrsep01",
+    linkedin: "https://linkedin.com/in/sep-alamouti",
+    twitter: "https://twitter.com/sepalamouti",
+    
+  
+
+
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
